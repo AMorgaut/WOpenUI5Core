4370c939bfc577eb6fb4ee16d5447c688e5e159c
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2015-03-07 17:41:13 +0100

    OpenUI5 Core

---
+++ designer.js
@@ -0,0 +1,40 @@
+(function(OpenUI5Core) {
+
+//    /* Default width and height of your widget */
+//    OpenUI5Core.setWidth('200');
+//    OpenUI5Core.setHeight('20');
+
+//    /* Define custom event for your widget */
+//    OpenUI5Core.addEvent('myEvent');
+
+//    /* Customize existing properties */
+//    OpenUI5Core.customizeProperty('test', {
+//        title: 'Test Static Value',
+//        description: 'Add a datasource to this property.'
+//    });
+
+//    /* Add a Label property */
+//    OpenUI5Core.addLabel({
+//        'defaultValue': '',
+//        'position': 'top'
+//    });
+
+//    /* Set the Design and Styles panels */
+//    OpenUI5Core.setPanelStyle({
+//        'fClass': true,
+//        'text': true,
+//        'background': true,
+//        'border': true,
+//        'sizePosition': true,
+//        'label': true,
+//        'disabled': ['border-radius']
+//    });
+
+//    /* Override widget's initialization */
+//    OpenUI5Core.prototype.init = function() {
+//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
+//    }
+
+});
+
+// For more information, refer to http://doc.wakanda.org/Wakanda/help/Title/en/page3870.html
